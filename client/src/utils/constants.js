172a8ba35943ffb9fb34da4233b74491aeceb52f
liyuwangluo <liyu2016520@163.com>
2022-06-28 16:25:41 +0800

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x7813179c210b77a00E513C90Ae8Fbec1c748Db38";
+export const contractABI = abi.abi;
---
+++ client/src/utils/dummyData.js
@@ -0,0 +1,12 @@
+
+export default [
+  {
+    id: 1,
+    url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2F0xzx.com%2Fwp-content%2Fuploads%2F2021%2F05%2F20210504-117.jpg&refer=http%3A%2F%2F0xzx.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1658989664&t=9a614e6952bd78490edefb5baa6f1c15",
+    message: "",
+    timestamp: "12/21/2021, 4:33:21 PM",
+    addressFrom: "0xCF8e569A97C423952DdFf902375C7C76549A6A90",
+    amount: "0.01",
+    addressTo: "0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE",
+  }
+];
